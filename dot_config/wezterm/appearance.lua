1df9bc4ae4651a8936f5a6c6fb71b4e32c8c5c8e
Raphael <raphael.t.ruban@gmail.com>
2024-07-31 01:07:16 +0200

    feat: adds keymaps to wezterm

--- dot_config/wezterm/appearance.lua
+++ dot_config/wezterm/appearance.lua
@@ -23,32 +23,76 @@ function module.apply_to_config(config)
   }
   config.font_size = 16
 
-  config.hide_tab_bar_if_only_one_tab = true
-  config.use_fancy_tab_bar = true
   config.window_decorations = 'RESIZE'
-
-  config.window_frame = {
-    -- The font used in the tab bar.
-    -- Roboto Bold is the default; this font is bundled
-    -- with wezterm.
-    -- Whatever font is selected here, it will have the
-    -- main font setting appended to it to pick up any
-    -- fallback fonts you may have used there.
-    font = wezterm.font { family = 'Iosevka Term SS14', weight = 'Bold', stretch = 'Expanded' },
-
-    -- The size of the font in the tab bar.
-    -- Default to 10.0 on Windows but 12.0 on other systems
-    font_size = 12.0,
-
-    -- The overall background color of the tab bar when
-    -- the window is focused
-    active_titlebar_bg = '#1E1F29',
-
-    -- The overall background color of the tab bar when
-    -- the window is not focused
-    inactive_titlebar_bg = '#1E1F29',
-  }
   config.window_padding = { top = 0, bottom = 0 }
+
+  -- Tab bar settings
+  config.hide_tab_bar_if_only_one_tab = false
+  config.tab_bar_at_bottom = true
+  config.use_fancy_tab_bar = false
+  -- Tab bar colors for Snazzy
+  config.colors = {
+    tab_bar = {
+      -- The color of the strip that goes along the top of the window
+      background = '#282a36',
+      -- The active tab is the one that has focus in the window
+      active_tab = {
+        bg_color = '#57c7ff', -- blue
+        fg_color = '#282a36', -- background color for contrast
+        intensity = 'Normal',
+      },
+      -- Inactive tabs are the tabs that do not have focus
+      inactive_tab = {
+        bg_color = '#282a36', -- background
+        fg_color = '#f1f1f0', -- white
+      },
+      -- Alternate styling when the mouse pointer moves over inactive tabs
+      inactive_tab_hover = {
+        bg_color = '#686868', -- bright black (grey)
+        fg_color = '#eff0eb', -- bright white
+        italic = true,
+      },
+      -- The new tab button that lets you create new tabs
+      new_tab = {
+        bg_color = '#282a36', -- background
+        fg_color = '#f1f1f0', -- white
+      },
+      -- Alternate styling when the mouse pointer moves over the new tab button
+      new_tab_hover = {
+        bg_color = '#5af78e', -- bright green
+        fg_color = '#282a36', -- background color for contrast
+        italic = true,
+      },
+    },
+  }
+  -- tmux status
+  wezterm.on('update-right-status', function(window, _)
+    local SOLID_LEFT_ARROW = ''
+    -- local ARROW_FOREGROUND = { Foreground = { Color = '#c6a0f6' } } -- Catppuccin
+    local ARROW_FOREGROUND = { Foreground = { Color = '#57c7ff' } } -- Snazzy
+    local prefix = ''
+
+    if window:leader_is_active() then
+      prefix = ' ' .. '🌊' -- utf8.char(0x1f30a) -- ocean wave
+      SOLID_LEFT_ARROW = '' -- utf8.char(0xe0b2)
+    end
+
+    if window:active_tab():tab_id() ~= 0 then
+      -- ARROW_FOREGROUND = { Foreground = { Color = '#1e2030' } } -- Catppuccin
+      ARROW_FOREGROUND = { Foreground = { Color = '#282a36' } } -- Snazzy
+    end -- arrow color based on if tab is first pane
+
+    window:set_left_status(wezterm.format {
+      -- { Background = { Color = '#b7bdf8' } }, -- Catppuccin
+      { Background = { Color = '#9aedfe' } }, -- Snazzy
+      { Text = prefix },
+      ARROW_FOREGROUND,
+      { Text = SOLID_LEFT_ARROW },
+    })
+  end)
+
+  -- Apple macOS style symbols
+  config.ui_key_cap_rendering = 'AppleSymbols'
 end
 
 -- return our module table
--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -7,9 +7,12 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 local appearance = require 'appearance'
 local hyperlink = require 'hyperlink'
+local keymaps = require 'keymaps'
 
 appearance.apply_to_config(config)
 hyperlink.apply_to_config(config)
+-- This sets up tmux like functionality and other keybindings.
+keymaps.apply_to_config(config)
 
 -- and finally, return the configuration to wezterm
 return config
