9cd33d6d6a0dfc68a10c306dc480b6175d67f0b4
Raphael <raphael.t.ruban@gmail.com>
2024-10-16 17:36:37 -0400

    feat: add ZenMode keymap

--- dot_config/nvim/lua/custom/plugins/zenmode.lua
+++ dot_config/nvim/lua/custom/plugins/zenmode.lua
@@ -6,4 +6,7 @@ return {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
   },
+  keys = {
+    { '<leader>tz', '<cmd>ZenMode<cr>', desc = 'Toggle [Z]enMode' },
+  },
 }
