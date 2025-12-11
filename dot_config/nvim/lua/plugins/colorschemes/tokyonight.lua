015ec3ef0edfc1445f0000b2638fa0bc6769a483
Raphael <raphael.t.ruban@gmail.com>
2025-12-10 22:00:48 -0800

    chore(nvim): add typing to tokyonight

--- dot_config/nvim/lua/plugins/colorschemes/tokyonight.lua
+++ dot_config/nvim/lua/plugins/colorschemes/tokyonight.lua
@@ -3,14 +3,18 @@
 return {
   'folke/tokyonight.nvim',
   priority = 1000,
+  ---@class tokyonight.Config
+  ---@field on_colors fun(colors: ColorScheme)
+  ---@field on_highlights fun(highlights: tokyonight.Highlights, colors: ColorScheme)
   opts = {
+    style = 'night',
     styles = {
       comments = { italic = false },
     },
   },
   config = function(_, opts)
     require('tokyonight').setup(opts)
-    vim.cmd.colorscheme 'tokyonight-night'
+    vim.cmd.colorscheme 'tokyonight'
   end,
 }
 -- vim: ts=2 sts=2 sw=2 et
